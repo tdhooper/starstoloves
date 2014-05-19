a1811b446b03b9edcd8e91b1fbe95739d55fcbb6
Thomas Hooper <tom@stainlessvision.com>
2014-05-19 22:47:45 +0100

    Add missing spotify and lfm dependencies

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -55,6 +55,17 @@ fi
 cp -p $PROJECT_DIR/etc/install/bashrc /home/vagrant/.bashrc
 su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache"
 
+# Install libspotify
+
+# from http://pyspotify.mopidy.com/en/latest/installation/
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/mopidy.list
+sudo apt-get update
+sudo apt-get install -y libspotify-dev
+
+#from http://stackoverflow.com/questions/21158317/ioerror-on-production-server
+sudo apt-get install libffi-dev
+
 # ---
 
 # postgresql setup for project
