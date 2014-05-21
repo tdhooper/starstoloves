f93a71ec12d84404acd2d0fce4e80a112f1d4d58
Thomas Hooper <tom@stainlessvision.com>
2014-05-21 20:48:53 +0100

    Break apart connection preprocessors and control flow from the view not the template

--- starstoloves/views.py
+++ starstoloves/views.py
@@ -8,68 +8,102 @@ import forms
 import middleware
 import spotify
 
-def lastfm_connection_processor(request):
-    lfmSession = request.session.get('lfmSession')
+
+def get_lastfm_session_context(lfmSession):
+    return {
+        'lfmUsername': lfmSession['name'],
+        'lfmDisconnectUrl': reverse(disconnectLastfm),
+    }
+
+def get_lastfm_connect_context():
+    return {
+        'lfmConnectUrl': reverse(connectLastfm)
+    }
+
+def negotiate_lastfm_connection(session, context):
+    lfmSession = session.get('lfmSession')
     if lfmSession:
-        return {
-            'lfmUsername': lfmSession['name'],
-            'lfmDisconnectUrl': reverse(disconnectLastfm),
-        }
-    return {'lfmConnectUrl': reverse(connectLastfm)}
-
-def spotify_connection_processor(request):
-    spSession = request.session.get('spSession')
-    if spSession:
-        return {
-            'spUsername': spSession['username'],
-            'spDisconnectUrl': reverse(disconnectSpotify),
-        }
-
-    if request.method == 'POST':
-        form = forms.SpotifyConnectForm(request.POST, spotifySession=request.spotifySession);
-        if form.is_valid():
-            userUri = form.cleaned_data.get('userUri')
-            user = request.spotifySession.get_user(userUri)
-            username = user.load().display_name
-            request.session['spSession'] = {
-                'username': username,
-                'userUri': userUri,
-            }
-            return spotify_connection_processor(request)
+        context.update(get_lastfm_session_context(lfmSession))
     else:
-        form = forms.SpotifyConnectForm(spotifySession=request.spotifySession);
+        context.update(get_lastfm_connect_context())
 
+def is_lastfm_connected(session):
+    return session.has_key('lfmSession')
+
+
+
+def get_spotify_session_context(spSession):
+    return {
+        'spUsername': spSession['username'],
+        'spDisconnectUrl': reverse(disconnectSpotify),
+    }
+
+def get_spotify_connect_context(form):
     return {
         'spForm': form,
         'spConnectUrl': reverse(index),
     }
+
+def negotiate_spotify_connection(request, context):
+    spSession = request.session.get('spSession')
+    if spSession:
+        context.update(get_spotify_session_context(spSession))
+    else:
+        if request.method == 'POST':
+            form = forms.SpotifyConnectForm(request.POST, spotifySession=request.spotifySession);
+            if form.is_valid():
+                userUri = form.cleaned_data.get('userUri')
+                user = request.spotifySession.get_user(userUri)
+                username = user.load().display_name
+                spSession = {
+                    'username': username,
+                    'userUri': userUri,
+                }
+                context.update(get_spotify_session_context(spSession))
+                request.session['spSession'] = spSession
+            else:
+                context.update(get_spotify_connect_context(form))
+        else:
+            form = forms.SpotifyConnectForm(spotifySession=request.spotifySession);
+            context.update(get_spotify_connect_context(form))
+
+def is_spotify_connected(session):
+    return session.has_key('spSession')
+
+
 
 @decorator_from_middleware(middleware.SpotifySession)
 @decorator_from_middleware(middleware.LastfmApp)
 def index(request):
     context = {}
 
-    context.update(lastfm_connection_processor(request))
-    context.update(spotify_connection_processor(request))
+    session = request.session
 
-    spSession = request.session.get('spSession')
-    if spSession and spSession['userUri']:
-        def get_tracks(item):
-            return {
-                'name': item.track.load().name,
-                'date': item.create_time,
-            }
-        
-        user = request.spotifySession.get_user(spSession['userUri'])
-        starred = user.load().starred
-        tracks = starred.load().tracks_with_metadata
-        context['starred'] = map(get_tracks, tracks)
+    negotiate_lastfm_connection(session, context)
+    negotiate_spotify_connection(request, context)
+
+    if is_lastfm_connected(session):
+        context['showSpotifyForm'] = True
+
+    if is_spotify_connected(session):
+        spSession = session.get('spSession')
+        if spSession and spSession['userUri']:
+            def get_tracks(item):
+                return {
+                    'name': item.track.load().name,
+                    'date': item.create_time,
+                }
+            
+            user = request.spotifySession.get_user(spSession['userUri'])
+            starred = user.load().starred
+            tracks = starred.load().tracks_with_metadata
+            context['starred'] = map(get_tracks, tracks)
 
     return render_to_response('index.html', context_instance=RequestContext(request, context))
 
 @decorator_from_middleware(middleware.LastfmApp)
 def connectLastfm(request):
-    if request.session.get('lfmSession'):
+    if is_lastfm_connected(request.session):
         return redirect(reverse(index))
     token = request.GET.get('token')
     if token:
@@ -80,16 +114,12 @@ def connectLastfm(request):
     return redirect(authUrl)
 
 def disconnectLastfm(request):
-    try:
+    if is_lastfm_connected(request.session):
         del request.session['lfmSession']
-    except KeyError:
-        pass
     return redirect(reverse(index))
 
 def disconnectSpotify(request):
-    try:
+    if is_spotify_connected(request.session):
         del request.session['spSession']
-    except KeyError:
-        pass
     return redirect(reverse(index))
     
