524ffc3bf0db7dac9d74dc5c8fd9345679d7027d
Thomas Hooper <tom@stainlessvision.com>
2014-06-17 01:56:18 +0100

    Include artist name in the track name string for the Last.fm search
    It seems to give better results combined, compared to when the
    artist is sent as the artist param.

--- starstoloves/tasks.py
+++ starstoloves/tasks.py
@@ -4,4 +4,4 @@ from celery import shared_task
 
 @shared_task
 def search_lastfm(lastfm_app, track_name, artist_name):
-    return lastfm_app.track.search(track_name, artist_name)
+    return lastfm_app.track.search(artist_name + ' ' + track_name)
