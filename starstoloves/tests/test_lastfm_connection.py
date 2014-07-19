ccf2855396921a6d2123c3d9c2c6dc56ee690740
Thomas Hooper <tom@stainlessvision.com>
2014-07-19 17:44:48 +0100

    Migrate LastfmConnectionHelper tests to pytest

--- starstoloves/tests/test_lastfm_connection.py
+++ starstoloves/tests/test_lastfm_connection.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock
 
-from django.test import TestCase
+import pytest
 
 from lastfm import lfm
 
@@ -8,120 +8,117 @@ from starstoloves.models import User, LastfmConnection
 from starstoloves.views.helpers.connection import MissingUserError
 from starstoloves.views.helpers.lastfm_connection import LastfmConnectionHelper
 
+pytestmark = pytest.mark.django_db
 
-class BaseTestLastfmConnection(TestCase):
+@pytest.fixture
+def app():
+    return MagicMock(spec=lfm.App).return_value
 
-    def setUp(self):
-        self.app = MagicMock(spec=lfm.App).return_value
-        self.connection = LastfmConnectionHelper(None, self.app)
+@pytest.fixture
+def user(request):
+    user = User(session_key='some_key')
+    user.save()
+    def fin():
+        user.delete()
+    request.addfinalizer(fin)
+    return user
 
+@pytest.fixture
+def connection_without_user(request, app):
+    return LastfmConnectionHelper(None, app)
 
-class BaseTestLastfmConnectionWithUser(BaseTestLastfmConnection):
+@pytest.fixture
+def connection_with_user(request, user, app):
+    return LastfmConnectionHelper(user, app)
 
-    def setUp(self):
-        super().setUp()
-        self.user = User(session_key='some_key')
-        self.user.save()
-        self.connection = LastfmConnectionHelper(self.user, self.app)
+@pytest.fixture(params=[True, False])
+def connection(request, user, app):
+    if (request.param):
+        return LastfmConnectionHelper(user, app)
+    else:
+        return LastfmConnectionHelper(None, app)
 
-    def tearDown(self):
-        self.user.delete()
+@pytest.fixture
+def fetch_user():
+    return User.objects.get(session_key='some_key')
 
+@pytest.fixture
+def fetch_connection(fetch_user, app):
+    return LastfmConnectionHelper(fetch_user, app)
 
-class BaseLastfmConnectionTests():
-
-    def test_connection_state_defaults_to_disconnected(self):
-        self.assertEqual(self.connection.get_connection_state(), self.connection.DISCONNECTED)
-
-    def test_connection_username_defaults_to_none(self):
-        self.assertIsNone(self.connection.get_username())
-
-    def test_connection_disconnect_is_a_noop(self):
-        self.connection.disconnect()
-
-    def test_get_auth_url_proxies_to_app(self):
-        def get_url(callback_url):
-            if (callback_url == 'some_callback'):
-                return 'some_auth_url'
-        self.app.auth.get_url.side_effect = get_url
-        auth_url = self.connection.get_auth_url('some_callback')
-        self.assertEqual(auth_url, 'some_auth_url')
-
-
-class TestLastfmConnectionWithoutUser(BaseTestLastfmConnection, BaseLastfmConnectionTests):
-
-    def test_connect_throws(self):
-        with self.assertRaises(MissingUserError):
-            self.connection.connect('some_token')
-
-
-class TestLastfmConnectionWithUser(BaseTestLastfmConnectionWithUser, BaseLastfmConnectionTests):
-    pass;
-
-
-class TestLastfmConnectionConnectSuccess(BaseTestLastfmConnectionWithUser):
-
-    def setUp(self):
-        super().setUp()
-        whenConnectSucceeds(self.app, self.connection)
-        self.user = User.objects.get(session_key='some_key')
-        self.connection = LastfmConnectionHelper(self.user, self.app)
-
-    def test_associates_a_LastfmConnection(self):
-        self.assertIsInstance(self.user.lastfm_connection, LastfmConnection)
-
-    def test_stores_the_username(self):
-        self.assertEqual(self.connection.get_username(), 'some_username')
-
-    def test_sets_the_connection_state_as_connected(self):
-        self.assertEqual(self.connection.get_connection_state(), self.connection.CONNECTED)
-
-
-class TestLastfmConnectionConnectFail(BaseTestLastfmConnectionWithUser):
-
-    def setUp(self):
-        super().setUp()
-        whenConnectFails(self.app, self.connection)
-        self.user = User.objects.get(session_key='some_key')
-        self.connection = LastfmConnectionHelper(self.user, self.app)
-
-    def test_associates_a_LastfmConnection(self):
-        self.assertIsInstance(self.user.lastfm_connection, LastfmConnection)
-
-    def test_sets_the_connection_state_as_failed(self):
-        self.assertEqual(self.connection.get_connection_state(), self.connection.FAILED)
-
-
-class TestLastfmConnectionDisconnect(BaseTestLastfmConnectionWithUser):
-
-    def setUp(self):
-        super().setUp()
-        whenConnectSucceeds(self.app, self.connection)
-        self.connection.disconnect()
-        self.user = User.objects.get(session_key='some_key')
-        self.connection = LastfmConnectionHelper(self.user, self.app)
-
-    def test_there_is_no_longer_an_associated_lastfm_connection(self):
-        with self.assertRaises(LastfmConnection.DoesNotExist):
-            self.user.lastfm_connection
-
-    def test_the_LastfmConnection_is_deleted(self):
-        with self.assertRaises(LastfmConnection.DoesNotExist):
-            LastfmConnection.objects.get(user=self.user)
-
-
-def whenConnectSucceeds(app, connection):
+@pytest.fixture
+def successful_connection(app, connection_with_user):
     def get_session(token):
         if (token == 'some_token'):
             return {'name': 'some_username'}
     app.auth.get_session.side_effect = get_session
-    connection.connect('some_token')
+    connection_with_user.connect('some_token')
 
-
-def whenConnectFails(app, connection):
+@pytest.fixture
+def failed_connection(app, connection_with_user):
     def get_session(token):
         if (token == 'some_token'):
             raise Exception()
     app.auth.get_session.side_effect = get_session
-    connection.connect('some_token')
+    connection_with_user.connect('some_token')
 
+@pytest.fixture
+def disconnected_connection(app, connection_with_user):
+    connection_with_user.disconnect()
+
+
+def test_state_defaults_to_disconnected(connection):
+    assert connection.get_connection_state() == connection.DISCONNECTED
+
+def test_username_defaults_to_none(connection):
+    assert connection.get_username() is None
+
+def test_disconnect_is_a_noop(connection):
+    connection.disconnect()
+
+def test_get_auth_url_proxies_to_app(connection, app):
+    def get_url(callback_url):
+        if (callback_url == 'some_callback'):
+            return 'some_auth_url'
+    app.auth.get_url.side_effect = get_url
+    auth_url = connection.get_auth_url('some_callback')
+    assert auth_url == 'some_auth_url'
+
+def test_connect_throws(connection_without_user):
+    with pytest.raises(MissingUserError):
+        connection_without_user.connect('some_token')
+
+
+@pytest.mark.usefixtures("successful_connection")
+class TestLastfmConnectionConnectSuccess():
+
+    def test_associates_a_LastfmConnection(self, fetch_user):
+        assert isinstance(fetch_user.lastfm_connection, LastfmConnection)
+
+    def test_stores_the_username(self, fetch_connection):
+        assert fetch_connection.get_username() == 'some_username'
+
+    def test_sets_the_connection_state_as_connected(self, fetch_connection):
+        assert fetch_connection.get_connection_state() == fetch_connection.CONNECTED
+
+
+@pytest.mark.usefixtures("failed_connection")
+class TestLastfmConnectionConnectFail():
+
+    def test_associates_a_LastfmConnection(self, fetch_user):
+        assert isinstance(fetch_user.lastfm_connection, LastfmConnection)
+
+    def test_sets_the_connection_state_as_failed(self, fetch_connection):
+        assert fetch_connection.get_connection_state() == fetch_connection.FAILED
+
+
+@pytest.mark.usefixtures("successful_connection", "disconnected_connection")
+class TestLastfmConnectionDisconnect():
+
+    def test_there_is_no_longer_an_associated_lastfm_connection(self, fetch_user):
+        with pytest.raises(LastfmConnection.DoesNotExist):
+            fetch_user.lastfm_connection
+
+    def test_the_LastfmConnection_is_deleted(self, fetch_user):
+        with pytest.raises(LastfmConnection.DoesNotExist):
+            LastfmConnection.objects.get(user=fetch_user)
