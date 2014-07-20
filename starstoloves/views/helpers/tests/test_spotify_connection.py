e665848513fc495af3f15612975394f0fc359e1b
Thomas Hooper <tom@stainlessvision.com>
2014-07-20 20:38:14 +0100

    Remove spotify connection tests already covered by common connection tests

--- starstoloves/views/helpers/tests/test_spotify_connection.py
+++ starstoloves/views/helpers/tests/test_spotify_connection.py
@@ -32,25 +32,12 @@ class BaseTestSpotifyConnectionWithUser(BaseTestSpotifyConnection):
 
 class BaseSpotifyConnectionTests():
 
-    def test_connection_state_defaults_to_disconnected(self):
-        self.assertEqual(self.connection.get_connection_state(), self.connection.DISCONNECTED)
-
-    def test_connection_username_defaults_to_none(self):
-        self.assertIsNone(self.connection.get_username())
-
     def test_connection_user_uri_defaults_to_none(self):
         self.assertIsNone(self.connection.get_user_uri())
 
-    def test_connection_disconnect_is_a_noop(self):
-        self.connection.disconnect()
-
 
 class TestSpotifyConnectionWithoutUser(BaseTestSpotifyConnection, BaseSpotifyConnectionTests):
-
-    def test_connect_throws(self):
-        with self.assertRaises(MissingUserError):
-            self.connection.connect('some_username')
-
+    pass
 
 class TestSpotifyConnectionWithUser(BaseTestSpotifyConnectionWithUser, BaseSpotifyConnectionTests):
     pass;
@@ -64,50 +51,8 @@ class TestSpotifyConnectionConnectSuccess(BaseTestSpotifyConnectionWithUser):
         self.user = User.objects.get(session_key='some_key')
         self.connection = SpotifyConnectionHelper(self.user, self.session)
 
-    def test_associates_a_SpotifyConnection(self):
-        self.assertIsInstance(self.user.spotify_connection, SpotifyConnection)
-
-    def test_stores_the_username(self):
-        self.assertEqual(self.connection.get_username(), 'some_username')
-
     def test_stores_the_user_uri(self):
         self.assertEqual(self.connection.get_user_uri(), 'spotify:user:some_username')
-
-    def test_sets_the_connection_state_as_connected(self):
-        self.assertEqual(self.connection.get_connection_state(), self.connection.CONNECTED)
-
-
-class TestSpotifyConnectionConnectFail(BaseTestSpotifyConnectionWithUser):
-
-    def setUp(self):
-        super().setUp()
-        whenConnectFails(self.session, self.connection)
-        self.user = User.objects.get(session_key='some_key')
-        self.connection = SpotifyConnectionHelper(self.user, self.session)
-
-    def test_associates_a_SpotifyConnection(self):
-        self.assertIsInstance(self.user.spotify_connection, SpotifyConnection)
-
-    def test_sets_the_connection_state_as_failed(self):
-        self.assertEqual(self.connection.get_connection_state(), self.connection.FAILED)
-
-
-class TestSpotifyConnectionDisconnect(BaseTestSpotifyConnectionWithUser):
-
-    def setUp(self):
-        super().setUp()
-        whenConnectSucceeds(self.session, self.connection)
-        self.connection.disconnect()
-        self.user = User.objects.get(session_key='some_key')
-        self.connection = SpotifyConnectionHelper(self.user, self.session)
-
-    def test_there_is_no_longer_an_associated_spotify_connection(self):
-        with self.assertRaises(SpotifyConnection.DoesNotExist):
-            self.user.spotify_connection
-
-    def test_the_SpotifyConnection_is_deleted(self):
-        with self.assertRaises(SpotifyConnection.DoesNotExist):
-            SpotifyConnection.objects.get(user=self.user)
 
 
 def whenConnectSucceeds(session, connection):
@@ -126,24 +71,3 @@ def whenConnectSucceeds(session, connection):
     session.get_user.side_effect = get_user
 
     connection.connect('some_username')
-
-
-def whenConnectFails(session, connection):
-    starred = MagicMock(spec=Playlist)
-    # starred.load.return_value = starred
-    # starred.tracks_with_metadata.side_effect = spotify.Error()
-    starred.load.side_effect = spotify.Error()
-
-    user = MagicMock(spec=SpotifyUser).return_value
-    user.load.return_value = user
-
-    user.starred = starred
-
-    def get_user(user_uri):
-        if user_uri == 'spotify:user:some_username':
-            return user
-    session.get_user.side_effect = get_user
-
-    connection.connect('some_username')
-
-
