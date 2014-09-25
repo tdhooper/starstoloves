127cd27eb2452a11131f6eef51420bedba1e8813
Thomas Hooper <tom@stainlessvision.com>
2014-09-25 16:24:11 +0100

    Add get_model methods to track repositories

--- starstoloves/lib/track/tests/test_spotify_track_repository.py
+++ starstoloves/lib/track/tests/test_spotify_track_repository.py
@@ -22,3 +22,13 @@ class TestGetOrCreate:
     def test_stores_track_and_artist_name_in_db(self):
         track = spotify_track_repository.get_or_create(track_name='some_track', artist_name='some_artist')
         assert SpotifyTrackModel.objects.filter(track_name='some_track', artist_name='some_artist').count() is 1
+
+
+class TestGetModel:
+
+    def test_returns_model(self):
+        track = spotify_track_repository.get_or_create(track_name='some_track', artist_name='some_artist')
+        track_model = spotify_track_repository.get_model(track)
+        assert isinstance(track_model, SpotifyTrackModel)
+        assert track_model.track_name == 'some_track'
+        assert track_model.artist_name == 'some_artist'
