0f53affcfb4bfb7569cf4a3a69387356a5c4855a
Thomas Hooper <tom@stainlessvision.com>
2014-09-25 21:41:30 +0100

    Fix missing session key on startup
    Sessions don't have a key until they've been saved

--- starstoloves/middleware.py
+++ starstoloves/middleware.py
@@ -4,6 +4,7 @@ class SessionUser:
 
     def process_request(self, request):
         user = None
+        request.session.save()
         session_key = request.session.session_key
         request.session_user = user_repository.from_session_key(session_key)
 
