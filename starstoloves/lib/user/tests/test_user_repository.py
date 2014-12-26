37eb0f4b53df6b42bd41620a63e7b1bf35229eec
Thomas Hooper <tom@stainlessvision.com>
2014-12-26 16:14:29 +0000

    Hide the user loved tracks db model
    - Use a business model instead

--- starstoloves/lib/user/tests/test_user_repository.py
+++ starstoloves/lib/user/tests/test_user_repository.py
@@ -4,9 +4,10 @@ import pytest
 
 from lastfm import lfm
 
-from starstoloves.models import LastfmTrack
 from starstoloves.models import User as UserModel
 from starstoloves import model_repository
+from starstoloves.lib.track import lastfm_track_repository
+from starstoloves.lib.track.lastfm_track import LastfmTrack
 from .. import user_repository
 from ..user import User
 
@@ -15,12 +16,12 @@ pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture
-def lastfm_track_models():
+def lastfm_tracks():
      return [
-        LastfmTrack.objects.create(
+        lastfm_track_repository.get_or_create(
+            url=track['url'],
             track_name=track['track_name'],
             artist_name=track['artist_name'],
-            url=track['url'],
         )
         for track in [{
             'track_name': 'some_track',
@@ -53,27 +54,31 @@ class TestFromSessionKey():
 class TestSave():
 
     @pytest.mark.usefixtures("patch_spotify_user")
-    def test_persists_loved_tracks(self, lastfm_track_models):
+    def test_persists_loved_tracks(self, lastfm_tracks):
         user = user_repository.from_session_key('some_key')
-        user.loved_tracks = lastfm_track_models
+        user.loved_tracks = lastfm_tracks
         user_repository.save(user)
 
         user = user_repository.from_session_key('some_key')
-        tracks = user.loved_tracks.values()
-        assert tracks[0]['track_name'] == 'some_track';
-        assert tracks[0]['artist_name'] == 'some_artist';
-        assert tracks[0]['url'] == 'some_url';
-        assert tracks[1]['track_name'] == 'another_track';
-        assert tracks[1]['artist_name'] == 'another_artist';
-        assert tracks[1]['url'] == 'another_url';
+        tracks = user.loved_tracks
+
+        assert isinstance(tracks[0], LastfmTrack)
+        assert tracks[0].track_name == 'some_track'
+        assert tracks[0].artist_name == 'some_artist'
+        assert tracks[0].url == 'some_url'
+
+        assert isinstance(tracks[1], LastfmTrack)
+        assert tracks[1].track_name == 'another_track'
+        assert tracks[1].artist_name == 'another_artist'
+        assert tracks[1].url == 'another_url'
 
 
 class TestDelete():
 
     @pytest.mark.usefixtures("patch_spotify_user")
-    def test_deletes_stored_user(self, lastfm_track_models):
+    def test_deletes_stored_user(self, lastfm_tracks):
         user = user_repository.from_session_key('some_key')
-        user.loved_tracks = lastfm_track_models
+        user.loved_tracks = lastfm_tracks
         user_repository.save(user)
 
         user_repository.delete(user)
--- starstoloves/lib/user/user_repository.py
+++ starstoloves/lib/user/user_repository.py
@@ -1,16 +1,26 @@
 from starstoloves.models import User as UserModel
 from starstoloves import model_repository
+from starstoloves.lib.track import lastfm_track_repository
 from .user import User
 
 def from_session_key(session_key):
     user_model, created = UserModel.objects.get_or_create(session_key=session_key)
-    loved_tracks = user_model.loved_tracks.all() or None
+    if user_model.loved_tracks.exists():
+        loved_tracks = [
+            lastfm_track_repository.from_model(track_model)
+            for track_model in user_model.loved_tracks.all()
+        ]
+    else:
+        loved_tracks = None
     return User(session_key, loved_tracks);
 
 def save(user):
     user_model, created = UserModel.objects.get_or_create(session_key=user.session_key)
     if user.loved_tracks:
-        user_model.loved_tracks = user.loved_tracks
+        user_model.loved_tracks = [
+            model_repository.from_lastfm_track(track)
+            for track in user.loved_tracks
+        ]
     user_model.save()
 
 def delete(user):
