3efe4799c6da164e9959da27d54940ae24255159
Thomas Hooper <tom@stainlessvision.com>
2016-05-08 15:04:57 +0100

    Use real api, it supports loving with a timestamp now

--- starstoloves/lib/lastfm.py
+++ starstoloves/lib/lastfm.py
@@ -6,5 +6,4 @@ from starstoloves import settings
 lastfm_app = lfm.App(
     key=settings.LASTFM['key'],
     secret=settings.LASTFM['secret'],
-    api_root='http://thirdparty.ws.past-loves-api.dev.audioscrobbler.com/2.0/',
 )
